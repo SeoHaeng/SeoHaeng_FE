c5ac8d8bf51b0995e8101ac589117b3859450d68
OrangeKim04 <queemkimu@naver.com>
2025-06-20 01:06:50 +0900

    feat: 북챌린지 페이지 1차 구현

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,29 @@ import { Tabs } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import { View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
+
+interface TextWithDefaultProps extends Text {
+  defaultProps?: { allowFontScaling?: boolean };
+}
+interface TextInputWithDefaultProps extends TextInput {
+  defaultProps?: { allowFontScaling?: boolean };
+}
+
+// Text 적용 : 시스템 폰트 크기를 무시하고 앱에서 지정한 크기를 사용함.
+(Text as unknown as TextWithDefaultProps).defaultProps =
+  (Text as unknown as TextWithDefaultProps).defaultProps || {};
+(Text as unknown as TextWithDefaultProps).defaultProps!.allowFontScaling =
+  false;
+
+// TextInput 적용 : 시스템 폰트 크기를 앱에서 지정
+(TextInput as unknown as TextInputWithDefaultProps).defaultProps =
+  (TextInput as unknown as TextInputWithDefaultProps).defaultProps || {};
+(
+  TextInput as unknown as TextInputWithDefaultProps
+).defaultProps!.allowFontScaling = false;
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
@@ -34,11 +54,9 @@ export default function RootLayout() {
           tabBarActiveTintColor: "#000000",
           tabBarInactiveTintColor: "#716C69",
           headerShown: false,
-          tabBarStyle: {
-            padding: 0,
-          },
           tabBarLabelStyle: {
             fontFamily: "SUIT-500",
+            fontSize: 11,
           },
         }}
       >

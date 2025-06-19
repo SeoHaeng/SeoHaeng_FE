775e04165af082296cf90440a70bae338aa6ec1a
OrangeKim04 <queemkimu@naver.com>
2025-06-19 14:42:33 +0900

    feat: 초기 루트 레이아웃 설정

---
+++ app/maru/bookmark.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Memory() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>공간책갈피</Text>
+    </View>
+  );
+}

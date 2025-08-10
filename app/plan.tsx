7f0516126d87cddd2d31a82c3387fee15a3ffcfd
OrangeKim04 <queemkimu@naver.com>
2025-08-10 13:12:06 +0900

    feat: 일정 메인 페이지와 여행 이름 수정 모달 생성

--- app/plan.tsx
+++ app/plan.tsx
@@ -2,12 +2,12 @@ import BackIcon from "@/components/icons/BackIcon";
 import { router } from "expo-router";
 import React, { useEffect, useRef, useState } from "react";
 import {
-    SafeAreaView,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from "react-native";
 
 export default function Plan() {

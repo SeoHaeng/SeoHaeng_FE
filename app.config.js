8733eefca87c2a584e6542562e1883b573cef535
OrangeKim04 <queemkimu@naver.com>
2025-09-07 05:13:32 +0900

    fix: 버전 코드 수정

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "서행",
     slug: "SeoHaeng_FE",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "seohaeng",
@@ -18,6 +18,7 @@ export default {
     },
     android: {
       package: "com.gyurijake.seohaengfe",
+      versionCode: 2,
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
       },
